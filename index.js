5252ae4b53b6a61fd86a65a680e29f5cc814d0ce
Dinu Infinity <dinuinfinity@gmail.com>
2025-10-28 23:25:29 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+function sizeHumanizer(bytes) {
+  const units = ["B", "KB", "MB", "GB", "TB"];
+  let i = 0;
+  while (bytes >= 1024 && i < units.length - 1) {
+    bytes /= 1024;
+    i++;
+  }
+  return `${bytes.toFixed(2)} ${units[i]}`;
+}
+
+module.exports = sizeHumanizer;
